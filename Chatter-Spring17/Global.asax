4119417497d6d94f2008b5189bda6314d7085863
lilykrichels <lily.krichels@gmail.com>
2017-05-30 13:34:41 -0400

    First Commit

---
+++ Chatter-Spring17/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chatter_Spring17.MvcApplication" Language="C#" %>
---
+++ Chatter-Spring17/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chatter_Spring17.Startup))]
+namespace Chatter_Spring17
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
