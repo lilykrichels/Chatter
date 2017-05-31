3b49159cf13b86aa3a032a5b80afa5206a872657
lilykrichels <lily.krichels@gmail.com>
2017-05-31 09:58:12 -0400

    Added/Took Away SSL

--- Chatter-Spring17/Controllers/HomeController.cs
+++ Chatter-Spring17/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Chatter_Spring17.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+         public ActionResult Index()
         {
             return View();
         }
